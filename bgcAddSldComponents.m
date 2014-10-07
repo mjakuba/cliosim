267f79ce0ef7b17de35aae4bbd7f85afef297b5a
jakuba <jakuba@porpentine>
2014-10-07 12:49:58 -0400

    added rudimentary checking for internal masses.  Recreated proposal results but with the thrusting down vehicle.

--- bgcAddSldComponents.m
+++ bgcAddSldComponents.m
@@ -1,5 +1,7 @@
 % 2014/10/03 18:18:39  Read these in from a the output of the sldwrks macro.
-fmacro = '/home/jakuba/Dropbox/Clio/vehicle/sld/tmp/CLIO-199-0000.SLDASM.txt';  % hard-coded for now.
+% 2014/10/06 20:43:17  Screwed up archived copy of first run with bogus vehicle.  Need to restore from dbox.
+%fmacro = '/home/jakuba/Dropbox/Clio/vehicle/sld/tmp/CLIO-199-0000.SLDASM.txt';  % hard-coded for now
+fmacro = '/home/jakuba/Dropbox/Clio/vehicle/sld/tmp/proposalVehicle.txt';
 %autoread('/home/jakuba/Dropbox/Clio/vehicle/sld/tmp/CLIO-199-0000.SLDASM.txt');
 % autoread won't work right now because of scientific notation and also doubles being interpreted as %d if the first
 % line happens to have spat out an integer.
@@ -12,31 +14,40 @@ fmacro = '/home/jakuba/Dropbox/Clio/vehicle/sld/tmp/CLIO-199-0000.SLDASM.txt';  
 % Will need to add assertions for internal parts (will show up in type field).
 for n=1:length(sld.componentName)
   
-   % I think I have this fixed, but in some cases seems like bodies (as opposed to parts) had custom material properties
-   % assigned and this catches those.
-   assert(abs(sld.mass(n)/sld.volume(n) - sld.density(n)) < eps(1e5)); 
+  c = bgcInitComponent(sld.componentName(n));  
+  if strcmp(sld.componentType(n),'solid')
 
-   c = bgcInitComponent(sld.componentName(n));
-   c.m = sld.mass(n);
-   c.V = sld.volume(n);
-   c.rho = sld.density(n);
-   c.alpha = sld.coeffThermalExpansion(n);
+    % I think I have this fixed, but in some cases seems like bodies (as opposed to parts) had custom material properties
+    % assigned and this catches those.
+    assert(abs(sld.mass(n)/sld.volume(n) - sld.density(n)) < eps(1e5)); 
+
+    c.m = sld.mass(n);
+    c.V = sld.volume(n);
+    c.rho = sld.density(n);
+    c.alpha = sld.coeffThermalExpansion(n);
    
-   % Deal with bulk modulus.
-   if strcmp(sld.componentType(n),'solid')
-     if ~isnan(sld.bulkModulus(n))
-       warning('Using custom bulk modulus for solid component.');
-       c.chi = 1/c.bulkModulus(n);  % @@@ not sure this is a good idea - at least warn that we are using a hard-coded
-                                    % custom property for some component as delivered.
-     else
-       bulkModulus = sld.elasticModulus(n)/(3*(1-2*sld.poissonsRatio(n)));
-       c.chi = 1/bulkModulus;
-     end
-   else
-     error('Unsupported component type.');
-   end
+    % Deal with bulk modulus.
+    if ~isnan(sld.bulkModulus(n))
+      warning('Using custom bulk modulus for solid component.');
+      c.chi = 1/c.bulkModulus(n);  % @@@ not sure this is a good idea - at least warn that we are using a hard-coded
+      % custom property for some component as delivered.
+    else
+      bulkModulus = sld.elasticModulus(n)/(3*(1-2*sld.poissonsRatio(n)));
+      c.chi = 1/bulkModulus;
+    end
+  
+     c.cp = sld.specificHeat(n);
+    
+  elseif strcmp(sld.componentType(n),'internal')
+    
+    c.m = sld.mass(n);
+    % All other values ignored and left at defaults.
    
-   c.cp = sld.specificHeat(n);
+  else
+   error('Unsupported component type.');
+ end
+   
+  
    
    
    % Cheating here - functionalize this later.
